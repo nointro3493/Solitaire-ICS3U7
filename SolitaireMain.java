3f8bca9e9b64a72e0a8b6bc2bd0cc447439ae10b
nointro3493 <95059950+nointro3493@users.noreply.github.com>
2021-12-19 21:12:53 -0500

    Add files via upload

---
+++ SolitaireMain.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+public class SolitaireMain {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Solitaire");
+	    frame.setSize(1200,600);
+	    frame.setVisible( true );
+	    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	}
+
+}
